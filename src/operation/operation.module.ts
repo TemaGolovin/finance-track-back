883fea20be04c1d055fe89be899cd76c6341666b
a.golovin <a.golovin@iq-adv.ru>
2025-04-23 22:45:45 +0500

    main | fix :missing providers

--- src/operation/operation.module.ts
+++ src/operation/operation.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { OperationController } from './operation.controller';
 import { OperationService } from './operation.service';
+import { OperationRepository } from './operation.repository';
+import { CategoryService } from 'src/category/category.service';
+import { CategoryRepository } from 'src/category/category.repository';
 
 @Module({
   controllers: [OperationController],
-  providers: [OperationService],
+  providers: [OperationService, OperationRepository, CategoryService, CategoryRepository],
 })
 export class OperationModule {}
